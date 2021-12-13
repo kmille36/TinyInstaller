e73fe60233f91e2c63b5dfd08717acbeab918f92
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-13 08:37:01 +0700

    Update ti.sh

--- ti.sh
+++ ti.sh
@@ -7,7 +7,7 @@ export ipGate=$4
 export DISK=$5
 export ipDNS='8.8.8.8'
 export setNet='0'
-export tiIso='https://github.com/4iTeam/TinyInstaller/raw/main/ti.iso'
+export tiIso='https://raw.githubusercontent.com/kmille36/TinyInstaller/main/ti.iso'
 
 
 [ "$EUID" -ne '0' ] && echo "Error:This script must be run as root!" && exit 1;
